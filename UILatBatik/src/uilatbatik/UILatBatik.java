a295188dea343c12d0903971d1972cbbc0cb2aa8
Hendro Purwoko <hendroprwk08@gmail.com>
2019-05-20 19:39:22 +0700

    ok

---
+++ UILatBatik/src/uilatbatik/UILatBatik.java
@@ -0,0 +1,11 @@
+package uilatbatik;
+
+public class UILatBatik {
+
+
+    public static void main(String[] args) {
+        BatikForm btf = new BatikForm();
+        btf.setVisible(true);
+    }
+    
+}
